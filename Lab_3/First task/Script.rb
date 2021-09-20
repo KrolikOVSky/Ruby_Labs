b384e7214962f41fd54731268d0d72dac0549aeb
KrolikOVSky <ovskykrolik@gmail.com>
2021-09-20 20:18:51 +0300

    Fix some bugs and cleanup code in ***Lab_3.1***

--- Lab_3/First task/Script.rb
+++ Lab_3/First task/Script.rb
@@ -1,49 +1,50 @@
+require '../../utilities'
+
 def read_from_file(path)
-  people = []
-  file = File.open(path)
-  file.readlines.each do |human|
-    temp = human.split("-")
-    people << [temp[0], temp[1].chomp("\n")]
-  end
-  file.close
-  return people
-end
-
-def rrr
-
+	people = []
+	file = File.open(path)
+	file.readlines.each do |human|
+		temp = human.split("-")
+		people << [temp[0], temp[1].chomp.to_i]
+	end
+	file.close
+	people
 end
 
 def write_to_file(content, path)
-  file = File.open(path, "w")
-  content.each do |item|
-    file.write "Name surname: #{item[0]}\t\tage: #{item[1]}\n"
-  end
-  file.close
+	file = File.open(path, "w")
+	content.each do |item|
+		file.write "Name surname: #{item[0]}\t\tage: #{item[1]}\n"
+	end
+	file.close
 end
 
 def get_data_from_user
-  puts "Please enter age of student or \"-1\" for exit"
-  return gets.chomp("\n").to_i
+	message_out "Please enter age of student or \"-1\" for exit"
+	gets.to_i
 end
 
-input_path = "/home/alex/RubymineProjects/Ruby_Labs/Lab_3/First task/input.txt"
-output_path = "/home/alex/RubymineProjects/Ruby_Labs/Lab_3/First task/result.txt"
+def main
+	input_path = "./input.txt"
+	output_path = "./result.txt"
 
-age = get_data_from_user
-truth_people = []
-people = read_from_file(input_path)
-while age != -1 do
-  flag = false
-  people.each do |human|
-    if human[1] === age.to_s
-      truth_people << human
-      flag = true
-    end
-  end
-  unless flag
-    puts "Don't have student with current age"
-  end
-  age = get_data_from_user
+	truth_people = []
+	people = read_from_file(input_path)
+	loop do
+		age = get_data_from_user
+		break if age == -1
+		flag = false
+		people.each do |human|
+			if human[1] === age
+				truth_people << human
+				flag = true
+			end
+		end
+		unless flag
+			message_out "Don't have student with current age"
+		end
+	end
+
+	write_to_file(truth_people, output_path)
+	truth_people
 end
-
-write_to_file(truth_people, output_path)
