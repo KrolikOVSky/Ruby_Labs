b283187fb5a82774cd4caa531fbb18e04dfd3280
KrolikOVSky <ovskykrolik@gmail.com>
2021-09-20 19:08:44 +0300

    Replace all the `puts` with custom output method

--- Lab_1/Script.rb
+++ Lab_1/Script.rb
@@ -1,30 +1,35 @@
+require '../utilities'
+
 def first_task
-	# puts "Please enter your Name"
-	name = gets.to_s
-	# puts "Please enter your Surname"
-	surname = gets.to_s
-	# puts "Please enter your Age"
+	message_out "Please enter your Name"
+	name = gets.chomp
+	message_out "Please enter your Surname"
+	surname = gets.chomp
+	message_out "Please enter your Age"
 	age = gets.to_i
 	if age < 18
-		# puts "Hello, #{name} #{surname}. You are less then 18 years, but starting programming is never early"
-		"Hello, #{name} #{surname}. You are less then 18 years, but starting programming is never early"
+		message_out "Hello, #{name} #{surname}. You are less then 18 years, but starting programming is never early"
 	else
-		# puts "Hello, #{name} #{surname}. This time is nice for learning programming"
-		"Hello, #{name} #{surname}. This time is nice for learning programming"
+		message_out "Hello, #{name} #{surname}. This time is nice for learning programming"
 	end
 end
 
 def second_task
-	# puts "Please enter first number: "
+	message_out "Please enter first number: "
 	first = gets.to_i
-	# puts "Please enter second number: "
+	message_out "Please enter second number: "
 	second = gets.to_i
 
-	if first == 20 || second == 20
-		# puts "Result = 20"
-		20
+	if first == 20
+		message_out "Result = #{second}"
+		second
+	elsif second == 20
+		message_out "Result = #{first}"
+		first
 	else
-		# puts "Result = #{first + second}"
+		message_out "Result = #{first + second}"
 		first + second
 	end
 end
+
+first_task
--- Lab_2/Script.rb
+++ Lab_2/Script.rb
@@ -1,31 +1,32 @@
+require "../utilities"
+
 def first_task
   def get_last_two_chars(string)
     string.to_s.reverse[0, 2].reverse
   end
 
-  # puts "Enter some word"
+  message_out "Enter some word"
   word = gets.to_s
 
   if get_last_two_chars(word) == "cs"
-    # puts (2 ** word.length).to_s
-    (2 ** word.length)
+    message_out 2 ** word.length
   else
-    # puts word.reverse
-    word.reverse
+    message_out word.reverse
   end
 
 end
 
 def second_task
-  # puts "Enter quantity of Pokemons"
+  message_out "Enter quantity of Pokemons"
   count = gets.to_i
   pokemons = []
   count.times do
-    # puts "Enter name of Pokemon"
-    name = gets.chomp("\n").to_s
-    # puts "Enter color of Pokemon"
-    color = gets.chomp("\n").to_s
+    message_out "Enter name of Pokemon"
+    name = gets.to_s
+    message_out "Enter color of Pokemon"
+    color = gets.to_s
     pokemons << { name => color }
   end
+  message_out pokemons
   pokemons
 end
